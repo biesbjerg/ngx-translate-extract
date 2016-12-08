85a1d04c96ffbf61c59648acf9057ce032778267
Kim Biesbjerg <kb@mindly.dk>
2016-12-08 14:12:43 +0100

    Add tests for PipeParser

---
+++ tests/pipe.parser.spec.ts
@@ -0,0 +1,33 @@
+import { expect } from 'chai';
+
+import { PipeParser } from '../src/parsers/pipe.parser';
+
+describe('PipeParser', () => {
+
+	const templateFilename: string = 'test.template.html';
+
+	let parser: PipeParser;
+
+	beforeEach(() => {
+		parser = new PipeParser();
+	});
+
+	it('should extract interpolated strings using translate pipe', () => {
+		const contents = `Hello {{ 'World' | translate }}`;
+		const messages = parser.process(templateFilename, contents);
+		expect(messages).to.deep.equal(['World']);
+	});
+
+	it('should extract interpolated strings using translate pipe in attributes', () => {
+		const contents = `<span attr="{{ 'Hello World' | translate }}"></span>`;
+		const messages = parser.process(templateFilename, contents);
+		expect(messages).to.deep.equal(['Hello World']);
+	});
+
+	it('should extract bound strings using translate pipe in attributes', () => {
+		const contents = `<span [attr]="'Hello World' | translate"></span>`;
+		const messages = parser.process(templateFilename, contents);
+		expect(messages).to.deep.equal(['Hello World']);
+	});
+
+});
