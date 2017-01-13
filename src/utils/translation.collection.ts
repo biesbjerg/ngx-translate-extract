c18b7dc9c7165d4c64a8ec164648f474adcc0b2d
Gustav Gahm <gustav.gahm@gmail.com>
2017-01-13 06:05:52 +0100

    Add CLI option to enable alphabetical sorting of extracted keys

--- src/utils/translation.collection.ts
+++ src/utils/translation.collection.ts
@@ -75,4 +75,17 @@ export class TranslationCollection {
 		return Object.keys(this.values).length === 0;
 	}
 
+	public sort(compareFn?: (a: string, b: string) => number): TranslationCollection {
+		if (!compareFn) {
+			// if no compare functions is provided use a case insensitive sorting function
+			compareFn = (a, b) => a.toLowerCase().localeCompare(b.toLowerCase());
+		}
+
+		let collection = new TranslationCollection();
+		let sortedKeys = this.keys().sort(compareFn);
+		sortedKeys.forEach((key) => {
+			collection = collection.add(key, this.get(key));
+		});
+		return collection;
+	}
 }
