39dcd6d42080abf5eedc75410d289c54ac157e91
Sebastian Zillessen (taazise2) <sebastian.zillessen1@swisscom.com>
2019-05-16 15:26:23 +0200

    Add minimal setup for travis and e2e tests (executing the complete cli and verifying the output files)

---
+++ e2e/cli/cli.spec.ts
@@ -0,0 +1,12 @@
+import cliHelper from './cli-helper';
+
+describe('Cli Test (E2E)', () => {
+	it('default format should extract successfully', async () => {
+		const cliResult = await cliHelper.execute();
+		cliResult.assertSuccess()
+			.assertNoErrors()
+			.assertOutputFileExists()
+			.assertOutputContainsKeys('"Here are some links to help you start:": "",')
+			.assertOutputContainsKeys('"welcome.title": ""');
+	});
+});
