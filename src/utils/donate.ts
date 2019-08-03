7eefd6c8d3eaccb7da82a21b020e6ec1c2af4d8d
Kim Biesbjerg <kb@mindly.dk>
2019-08-03 11:30:30 +0200

    fix donation message colors

--- src/utils/donate.ts
+++ src/utils/donate.ts
@@ -3,17 +3,15 @@ import * as boxen from 'boxen';
 import * as terminalLink from 'terminal-link';
 
 const url = 'https://donate.biesbjerg.com';
-const text = `
-If this tool saves you time, please consider making a
-donation towards the continued maintainence and development:
+const link = terminalLink(url, url);
+const message = `
+If this tool saves you or your company time, please consider making a
+donation to support my work and the continued maintainence and development:
 
-${yellow(terminalLink(url, url))}
-`;
+${yellow(link)}`;
 
-export const donateMessage = boxen(text.trim(), {
+export const donateMessage = boxen(message.trim(), {
 	padding: 1,
 	margin: 0,
-	borderColor: 'yellow',
-	backgroundColor: 'black',
 	dimBorder: true
 });
