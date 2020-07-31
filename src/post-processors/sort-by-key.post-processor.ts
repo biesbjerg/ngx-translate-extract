171402a5a2849abfc3c7f694dbde7ff8429c7ad4
BART! <bart@bartweb.cz>
2020-07-31 14:10:27 +0200

    feat(sorting): case-insensitive sorting method

--- src/post-processors/sort-by-key.post-processor.ts
+++ src/post-processors/sort-by-key.post-processor.ts
@@ -1,10 +1,21 @@
 import { TranslationCollection } from '../utils/translation.collection';
 import { PostProcessorInterface } from './post-processor.interface';
 
+export type SortOptions = '' | 'case-insensitive';
+
 export class SortByKeyPostProcessor implements PostProcessorInterface {
 	public name: string = 'SortByKey';
 
+	constructor(private sortOptions: SortOptions) { }
+
 	public process(draft: TranslationCollection, extracted: TranslationCollection, existing: TranslationCollection): TranslationCollection {
-		return draft.sort();
+		if (this.sortOptions === 'case-insensitive') {
+			return draft.sort((a, b) => {
+				return a.toLowerCase().localeCompare(b.toLowerCase());
+			});
+		} else {
+			return draft.sort();
+		}
 	}
 }
+
--- tests/post-processors/sort-by-key.post-processor.spec.ts
+++ tests/post-processors/sort-by-key.post-processor.spec.ts
@@ -4,28 +4,51 @@ import { PostProcessorInterface } from '../../src/post-processors/post-processor
 import { SortByKeyPostProcessor } from '../../src/post-processors/sort-by-key.post-processor';
 import { TranslationCollection } from '../../src/utils/translation.collection';
 
+const mock = {
+	ZZ: 'last value',
+	a: 'a value',
+	'9': 'a numeric key',
+	b: 'another value'
+};
+
+
 describe('SortByKeyPostProcessor', () => {
 	let processor: PostProcessorInterface;
+	let collection: TranslationCollection;
+	let extracted: TranslationCollection;
+	let existing: TranslationCollection;
 
 	beforeEach(() => {
-		processor = new SortByKeyPostProcessor();
+		collection = new TranslationCollection(mock);
+		extracted = new TranslationCollection();
+		existing = new TranslationCollection();
 	});
 
-	it('should sort keys alphanumerically', () => {
-		const collection = new TranslationCollection({
-			z: 'last value',
-			a: 'a value',
-			'9': 'a numeric key',
-			b: 'another value'
-		});
-		const extracted = new TranslationCollection();
-		const existing = new TranslationCollection();
+	it('should sort keys alphanumerically (case sensitive)', () => {
+		processor = new SortByKeyPostProcessor('');
+		const sorted = processor.process(collection, extracted, existing).values;
 
-		expect(processor.process(collection, extracted, existing).values).to.deep.equal({
+		const sortedOutput = {
+			'9': 'a numeric key',
+			ZZ: 'last value',
+			a: 'a value',
+			b: 'another value'
+		};
+
+		expect(JSON.stringify(sorted)).to.deep.equal(JSON.stringify(sortedOutput));
+	});
+
+	it('should sort keys alphanumerically (case insensitive)', () => {
+		processor = new SortByKeyPostProcessor('case-insensitive');
+		const sorted = processor.process(collection, extracted, existing).values;
+
+		const sortedOutput = {
 			'9': 'a numeric key',
 			a: 'a value',
 			b: 'another value',
-			z: 'last value'
-		});
+			ZZ: 'last value'
+		};
+
+		expect(JSON.stringify(sorted)).to.deep.equal(JSON.stringify(sortedOutput));
 	});
 });
