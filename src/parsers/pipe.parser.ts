0c9dab6e5bc74933ac5554a9abc8a623fb4691dc
venraij <Coolermaster08>
2021-11-15 21:31:51 +0100

    final fixes

--- src/parsers/pipe.parser.ts
+++ src/parsers/pipe.parser.ts
@@ -10,7 +10,7 @@ import {
 	LiteralMap,
 	LiteralArray,
 	Interpolation,
-	MethodCall
+	Call
 } from '@angular/compiler';
 
 import { ParserInterface } from './parser.interface.js';
@@ -132,7 +132,7 @@ export class PipeParser implements ParserInterface {
 			return this.getTranslatablesFromAsts(ast.expressions);
 		}
 
-		if (ast instanceof MethodCall) {
+		if (ast instanceof Call) {
 			return this.getTranslatablesFromAsts(ast.args);
 		}
 
